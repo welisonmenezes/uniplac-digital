3b0ce42e28479f31371413b640840b694ca88d7e
welisonmenezes <welisonmenezes@hotmail.com>
2019-09-05 12:38:31 -0300

    Add reacstrap and test a modal example

--- web-development/aplication/frontend/src/Components/Admin/AdminPosts/PostForm/PostForm.js
+++ web-development/aplication/frontend/src/Components/Admin/AdminPosts/PostForm/PostForm.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class PostForm extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            modal: false
+          };
 
         console.log(props);
+    }
+
+    toggle = () => {
+        this.setState(prevState => ({
+            modal: !prevState.modal
+        }));
     }
 
     render() {
@@ -16,6 +25,19 @@ class PostForm extends Component {
                     <div className="row">
                         <div className="col-md-12">
                             <h1>Admin post form</h1>
+                            <Button onClick={this.toggle} color="danger">Danger!</Button>
+
+                            <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                                <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                                <ModalBody>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </ModalBody>
+                                <ModalFooter>
+                                    <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+                                    <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                                </ModalFooter>
+                            </Modal>
+
                         </div>
                     </div>
                 </div>
