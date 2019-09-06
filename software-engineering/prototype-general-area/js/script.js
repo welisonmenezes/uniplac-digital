6d59e5576f1d9b2c964eb5ef2afe256641de41a5
Leonardo Salomon <leonardoperdona@hotmail.com>
2019-09-05 21:38:42 -0300

    Update carousel

--- software-engineering/prototype-general-area/js/script.js
+++ software-engineering/prototype-general-area/js/script.js
@@ -3,14 +3,45 @@ $(".slideshow").slick({
     autoplay: true,
     autoplaySpeed: 100,
     infinite: true,
-    fade: true
+    fade: true,
+    pauseOnHover: false,
+    pauseOnFocus: false
   });
 
   $(".carousel").slick({
     lazyLoad: 'ondemand',
     autoplay: true,
-    autoplaySpeed: 500,
+    autoplaySpeed: 1500,
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
