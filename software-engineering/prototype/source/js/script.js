f5e18f9f803470ec4d675278e5cb3d14d554efe0
welisonmenezes <welisonmenezes@hotmail.com>
2019-09-09 13:14:14 -0300

    Santatize prototypes and unify them, admin and public

---
+++ software-engineering/prototype/source/js/script.js
@@ -0,0 +1,47 @@
+$(".slideshow").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    infinite: true,
+    fade: true,
+    pauseOnHover: false,
+    pauseOnFocus: false
+});
+
+$(".carousel").slick({
+    lazyLoad: 'ondemand',
+    autoplay: true,
+    autoplaySpeed: 4000,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
