ad8947dcdb814461b546c28681747efd539cf6cf
welisonmenezes <welisonmenezes@hotmail.com>
2019-09-24 20:09:05 -0300

    Add masks to phone and numbers

---
+++ web-development/application/static/admin/js/custom.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    // mascara para telefone
+    function inputHandler(masks, max, event) {
+        var c = event.target;
+        var v = c.value.replace(/\D/g, '');
+        var m = c.value.length > max ? 1 : 0;
+        VMasker(c).unMask();
+        VMasker(c).maskPattern(masks[m]);
+        c.value = VMasker.toPattern(v, masks[m]);
+    }
+    var telMask = ['(99) 9999-99999', '(99) 99999-9999'];
+    var tel = document.querySelector('.mask-phone');
+    VMasker(tel).maskPattern(telMask[0]);
+    tel.addEventListener('input', inputHandler.bind(undefined, telMask, 14), false);
+
+
+    // máscara para números
+    VMasker(document.querySelector('.mask-number')).maskNumber();
+});
