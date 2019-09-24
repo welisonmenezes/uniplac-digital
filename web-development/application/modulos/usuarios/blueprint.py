adc61118ec4f420dc84e8a16d228230c963899c9
welisonmenezes <welisonmenezes@hotmail.com>
2019-09-24 15:23:22 -0300

    Implement user edition

--- web-development/application/modulos/usuarios/blueprint.py
+++ web-development/application/modulos/usuarios/blueprint.py
@@ -3,7 +3,7 @@ from flask import current_app, Blueprint, render_template, request, url_for, fla
 from app import bcrypt
 from modulos.usuarios.formularios import UsuarioForm
 from database.Model import db, User
-from modulos.usuarios.validations import validateUserData
+from modulos.usuarios.validations import validateUserToCreate, validateUserToUpdate
 
 usuarioBP = Blueprint('usuarios', __name__, url_prefix='/admin/usuarios', template_folder='templates', static_folder='static')
 
@@ -20,7 +20,7 @@ def cadastrar():
     form = UsuarioForm(request.form)
     if form.validate_on_submit():
         try:
-            if validateUserData(form):
+            if validateUserToCreate(form):
                 user = User(
                     form.first_name.data,
                     form.last_name.data,
@@ -45,21 +45,37 @@ def cadastrar():
 
 @usuarioBP.route('/editar/<int:id>', methods=['GET', 'POST'])
 def editar(id):
-    print(id)
+    user = User.query.filter((User.id==id)).first()
+    if not user:
+        flash('O usuário não existe', 'info')
+        return redirect(url_for('usuarios.index'))
     titulo = 'Editar usuário'
     if request.form:
         form = UsuarioForm(request.form)
     else:
         form = UsuarioForm()
-        form.first_name.data = 'Welison'
-        form.last_name.data = 'Menezes'
-        form.registry.data = '111111'
-        form.role.data = 'admin'
-        form.email.data = 'welison@email.com'
-        form.phone.data = '2222-3333'
+        fillForm(form, user)
+    # remove validação da senha
     form.password.validators = []
     if form.validate_on_submit():
-        print('valido')
+        try:
+            if validateUserToUpdate(form, user):
+                user.first_name = form.first_name.data
+                user.last_name = form.last_name.data
+                user.registry = form.registry.data
+                user.role = form.role.data
+                user.email = form.email.data
+                user.phone = form.phone.data
+                if (form.image_id.data != ''):
+                    user.image_id = form.image_id.data
+                if (form.password.data != ''):
+                    user.password = bcrypt.generate_password_hash(form.password.data)
+                db.session.commit()
+                flash('Usuário editado com sucesso', 'success')
+                return redirect(url_for('usuarios.editar', id=id))
+        except:
+            db.session.rollback()
+            flash('Erro ao tentar editar o usuário', 'danger')
     return render_template('usuarios/formulario.html', titulo=titulo, form=form, mode='editar'), 200
 
 
@@ -68,3 +84,15 @@ def deletar():
     titulo = 'Deseja realmente excluir o usuário [0002223]'
     return render_template('usuarios/deletar.html', titulo=titulo), 200
 
+
+# popula os campos do formuário
+def fillForm(form, user):
+    form.first_name.data = user.first_name
+    form.last_name.data = user.last_name
+    form.registry.data = user.registry
+    form.role.data = user.role
+    form.email.data = user.email
+    form.phone.data = user.phone
+    form.image_id.data = None
+    if (user.image_id != ''):
+        form.image_id.data = user.image_id
--- web-development/application/modulos/usuarios/validations.py
+++ web-development/application/modulos/usuarios/validations.py
@@ -1,7 +1,7 @@
 from flask import flash
 from database.Model import User
 
-def validateUserData(form):
+def validateUserToCreate(form):
     try:
         userWithRegistry = User.query.filter((User.registry == form.registry.data)).first()
         if userWithRegistry:
@@ -12,6 +12,26 @@ def validateUserData(form):
         if userWithEmail:
             flash('O email informado já foi cadastrado', 'danger')
             return False
+
+        return True
+    except:
+        flash('Erro ao tentar validar o usuário', 'danger')
+        return False
+
+
+def validateUserToUpdate(form, user):
+    try:
+        if (user.registry != form.registry.data):
+            userWithRegistry = User.query.filter((User.registry == form.registry.data)).first()
+            if userWithRegistry:
+                flash('A matrícula informada já foi cadastrada', 'danger')
+                return False
+        
+        if (user.email != form.email.data):
+            userWithEmail = User.query.filter((User.email == form.email.data)).first()
+            if userWithEmail:
+                flash('O email informado já foi cadastrado', 'danger')
+                return False
 
         return True
     except:
