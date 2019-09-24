6115dfed132a3b9e53291cd9205468645d335afe
Leonardo Salomon <leonardoperdona@hotmail.com>
2019-09-24 20:07:39 -0300

    Deleted settings view details screen

--- web-development/application/modulos/configuracoes/blueprint.py
+++ web-development/application/modulos/configuracoes/blueprint.py
@@ -3,10 +3,6 @@ from flask import current_app, Blueprint, render_template, request, url_for
 
 configuracaoBP = Blueprint('configuracoes', __name__, url_prefix='/admin/configuracoes', template_folder='templates', static_folder='static')
 
-@configuracaoBP.route('/')
-def index():
-    return render_template('/configuracoes/index.html'), 200
-
 @configuracaoBP.route('/editar')
 def editar():
     return render_template('/configuracoes/formulario.html'), 200
