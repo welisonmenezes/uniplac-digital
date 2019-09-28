90b5f8451a0773621286c0ea56847837dec125b6
welisonmenezes <welisonmenezes@hotmail.com>
2019-09-28 14:17:15 -0300

    Implement form contact process and send the email

--- web-development/application/modulos/site/blueprint.py
+++ web-development/application/modulos/site/blueprint.py
@@ -1,7 +1,8 @@
 import os
-from flask import current_app, Blueprint, render_template, request, url_for
+from flask import current_app, Blueprint, render_template, request, url_for, flash, redirect
+from flask_mail import Message
+from app import mail
 from modulos.site.formularios import ContactForm
-
 
 siteBP = Blueprint('site', __name__, url_prefix='/', template_folder='templates', static_folder='static')
 
@@ -46,12 +47,23 @@ def avisos_detalhes():
     return render_template('/site/detalhes.html', titulo=titulo), 200
 
 
- 
 @siteBP.route('/contato', methods=['GET','POST'])
 def contato():
     form = ContactForm(request.form)
     if form.validate_on_submit():
-        print('valido')
+        try:
+            msg = Message('Mensagem de - ' + form.name.data, sender='cntato@uniplacdigital.com.br', recipients=['uniplacdigital@gmail.com'])
+            msg.html = "<h1>"+ form.assunto.data +"</h1>"
+            msg.html += "<ul>"
+            msg.html += "<li><b>Nome: </b> "+ form.name.data +"</li>"
+            msg.html += "<li><b>Email: </b> "+ form.email.data +"</li>"
+            msg.html += "<li><b>Descrição: </b> "+ form.text.data +"</li>"
+            msg.html += "</ul>"
+            mail.send(msg)
+            flash('Mensagem enviada com sucesso.', 'success')
+            return redirect(url_for('site.contato', _anchor='formulario'))
+        except:
+            flash('Descuple, ocorreu um problema ao tentar enviar sua mensagem.', 'warning')
         
     return render_template('site/contato.html', form=form), 200
 
