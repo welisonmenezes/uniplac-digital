cb7b2b224aa919bc52febaeb2a8bea8bd0cac876
BielSteve <gabrielsteve@uniplaclages.edu.br>
2019-09-29 19:28:10 -0300

    Update Crud

--- web-development/application/modulos/configuracoes/blueprint.py
+++ web-development/application/modulos/configuracoes/blueprint.py
@@ -16,62 +16,81 @@ def cadastrar():
     
     titulo = 'Cadastrar Configuracao'
     form = ConfiguracaoForm(request.form)
+    configuration = Configuration.query.first()
     
-    
-    if form.validate_on_submit():
-        configuration = Configuration.query.first()  
-        try:
-                
-                # cria a configuracao com os dados do formulário
-                configuration = Configuration(
-                    form.name.data,
-                    form.description.data,
-                    form.phone.data,
-                    form.email.data,
-                    form.address.data,
-                    form.schedules.data,
-                    None
-                )
-                if form.image_id.data != '':
-                    configuration.image_id = form.image_id.data
 
-                # adiciona e commita a configuracao na base de dadso
-                db.session.add(configuration)
+    if configuration:
+        form.name.data = form.name
+        form.description.data = form.description
+        form.phone.data = form.phone
+        form.email.data = form.email
+        form.address.data = form.address
+        form.schedules.data = form.schedules
+        form.image_id.data = None
+        if (configuration.image_id != ''):
+            form.image_id.data = configuration.image_id
+
+    if form.validate_on_submit():
+        if configuration:        
+
+            try:
+                
+                   
+
+                # atualiza as configurações recuperado  com os dados do formulário
+                configuration.name = form.name.data
+                configuration.description = form.description.data
+                configuration.phone = form.phone.data
+                configuration.email = form.email.data
+                configuration.address = form.address.data
+                configuration.schedules = form.schedules.data
+                configuration.image_id = None
+                if (form.image_id.data != ''):
+                    configuration.image_id = form.image_id.data
+                
+
+            # commita os dados na base de dados
                 db.session.commit()
 
-                # flash message e redireciona pra mesma tela para limpar o objeto request
-                flash('Configuração criada com sucesso', 'success')
+            # flash message e redireciona pra mesma tela para limpar o objeto request
+                flash('Configuração editada com sucesso', 'success')
                 return redirect(url_for('configuracoes.cadastrar'))
-        except:
-            # remove qualquer vestígio do usuário da sessin e flash message 
-            db.session.rollback()
-            flash('Erro ao tentar cadastrar a Configuração', 'danger')
-        else:
-            try:
-                if (form, configuration):
-
-                # atualiza o usuário recuperado pelo id com os dados do formulário
-                    configuration.name = form.name.data
-                    configuration.description = form.description.data
-                    user.phone = form.phone.data
-                    user.email = form.email.data
-                    user.address = form.address.data
-                    user.schedules = form.schedules.data
-                    user.image_id = None
-                    if (form.image_id.data != ''):
-                        user.image_id = form.image_id.data
-                
-
-                # commita os dados na base de dados
-                    db.session.commit()
-
-                # flash message e redireciona pra mesma tela para limpar o objeto request
-                    flash('Configuração editada com sucesso', 'success')
-                    return redirect(url_for('configuracoes.editar'))
             except:
                 # remove qualquer vestígio da configuração da sessin e flash message
                 db.session.rollback()
                 flash('Erro ao tentar editar o configuração', 'danger')
+
+
+        else:  
+            if configuration: 
+                try:
+                    
+                    # cria a configuracao com os dados do formulário
+                    configuration = Configuration(
+                        form.name.data,
+                        form.description.data,
+                        form.phone.data,
+                        form.email.data,
+                        form.address.data,
+                        form.schedules.data,
+                        None
+                    )
+                    if form.image_id.data != '':
+                        configuration.image_id = form.image_id.data
+
+                    # adiciona e commita a configuracao na base de dadso
+                    db.session.add(configuration)
+                    db.session.commit()
+
+                    # flash message e redireciona pra mesma tela para limpar o objeto request
+                    flash('Configuração criada com sucesso', 'success')
+                    return redirect(url_for('configuracoes.cadastrar'))
+                except:
+                # remove qualquer vestígio de configuração da sessin e flash message 
+                    db.session.rollback()
+                    flash('Erro ao tentar cadastrar a Configuração', 'danger')
+        
+            
             
             
     return render_template('configuracoes/formulario.html', titulo=titulo, form=form, mode='cadastrar'), 200
