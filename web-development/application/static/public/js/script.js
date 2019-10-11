c96ec15ac1503070a959441d4d82fa40773f815b
welisonmenezes <welisonmenezes@hotmail.com>
2019-10-11 16:12:59 -0300

    Highlight site active links

--- web-development/application/static/public/js/script.js
+++ web-development/application/static/public/js/script.js
@@ -43,3 +43,36 @@ $(".carousel").slick({
         }
     ]
 });
+
+
+
+
+(function ($) {
+    'use strict';
+    $(function () {
+        var current = location.pathname;
+
+        function addActiveClass(element) {
+            console.log(current)
+            if (current.includes(element.attr('href'))) {
+                if (!element.parent().hasClass('first')) {
+                    element.parent().addClass('active');
+                } else {
+                    if (current === '/') {
+                        element.parent().addClass('active');
+                    }
+                }
+            }
+        }
+
+        //var current = location.pathname.split("/").slice(-1)[0].replace(/^\/|\/$/g, '');
+        var ls = location.pathname.split("/");
+        //var current = ls[1] + '/' + ls[2];
+        //console.log(ls);
+        $('.navbar-nav li a').each(function () {
+            var $this = $(this);
+            addActiveClass($this);
+        })
+
+    });
+})(jQuery);
