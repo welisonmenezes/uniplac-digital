43656bb467621a66c62e09756226bf3a81358f1b
Leonardo Salomon <leonardoperdona@hotmail.com>
2019-10-14 23:51:25 -0300

    Showing post in single post

--- web-development/application/modulos/site/blueprint.py
+++ web-development/application/modulos/site/blueprint.py
@@ -5,7 +5,7 @@ from flask_mail import Message
 from sqlalchemy import desc, and_, or_
 from app import mail
 from modulos.site.formularios import ContactForm
-from database.Model import Configuration, Category, Post, ConfigurationImage
+from database.Model import Configuration, Category, Post, ConfigurationImage, User
 
 siteBP = Blueprint('site', __name__, url_prefix='/', template_folder='templates', static_folder='static')
 current_datetime = datetime.now()
@@ -54,14 +54,19 @@ def noticias():
     return render_template('site/posts.html', posts=posts, notices=notices, paginate=paginate, currentPage=page, name=name, genre=genre, category=category, titulo=titulo, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted), 200
 
 
-@siteBP.route('/noticias/detalhes')
-def noticias_detalhes():
+@siteBP.route('/noticias/<int:id>')
+def noticias_detalhes(id):
     titulo = 'Notícias' 
     configuration = Configuration.query.first()
     categories = Category.query.order_by(desc(Category.id)).all()
     categories_highlighted = Category.query.filter((Category.is_highlighted==1)).order_by(desc(Category.id)).all()
     
-    return render_template('/site/detalhes.html', titulo=titulo, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted), 200
+    post = Post.query.filter(and_(Post.id==id, Post.genre=='news')).first()
+    user = User.query.filter(User.id==post.user_id).first()
+    category = Category.query.filter(Category.id==post.category_id).first()
+    notices = Post.query.filter(and_(Post.entry_date <= current_datetime, Post.departure_date >= current_datetime, Post.genre=='notice', Post.status=='approved')).order_by(desc(Post.id)).limit(6)
+
+    return render_template('/site/detalhes.html', post=post, user=user, category=category, notices=notices, titulo=titulo, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted), 200
 
 
 @siteBP.route('/anuncios')
@@ -95,14 +100,19 @@ def anuncios():
     return render_template('site/posts.html', posts=posts, notices=notices, category=category, name=name, genre=genre, paginate=paginate, currentPage=page, titulo=titulo, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted), 200
 
 
-@siteBP.route('/anuncios/detalhes')
-def anuncios_detalhes():
+@siteBP.route('/anuncios/<int:id>')
+def anuncios_detalhes(id):
+    titulo = 'Anuncios'
     configuration = Configuration.query.first()
     categories = Category.query.order_by(desc(Category.id)).all()
     categories_highlighted = Category.query.filter((Category.is_highlighted==1)).order_by(desc(Category.id)).all()
     
-    titulo = 'Anuncios'
-    return render_template('/site/detalhes.html', titulo=titulo, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted), 200
+    post = Post.query.filter(and_(Post.id==id, Post.genre=='ad')).first()
+    user = User.query.filter(User.id==post.user_id).first()
+    category = Category.query.filter(Category.id==post.category_id).first()
+    notices = Post.query.filter(and_(Post.entry_date <= current_datetime, Post.departure_date >= current_datetime, Post.genre=='notice', Post.status=='approved')).order_by(desc(Post.id)).limit(6)
+
+    return render_template('/site/detalhes.html', titulo=titulo, post=post, user=user, category=category, notices=notices, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted), 200
 
 
 @siteBP.route('/avisos')
@@ -134,14 +144,19 @@ def avisos():
     return render_template('site/posts.html', posts=posts, category=category, name=name, genre=genre, paginate=paginate, currentPage=page, titulo=titulo, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted), 200
 
 
-@siteBP.route('/avisos/detalhes')
-def avisos_detalhes():
+@siteBP.route('/avisos/<int:id>')
+def avisos_detalhes(id):
+    titulo = 'Avisos'
     configuration = Configuration.query.first()
     categories = Category.query.order_by(desc(Category.id)).all()
     categories_highlighted = Category.query.filter((Category.is_highlighted==1)).order_by(desc(Category.id)).all()
    
-    titulo = 'Avisos'
-    return render_template('/site/detalhes.html', titulo=titulo, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted), 200
+    post = Post.query.filter(and_(Post.id==id, Post.genre=='notice')).first()
+    user = User.query.filter(User.id==post.user_id).first()
+    category = Category.query.filter(Category.id==post.category_id).first()
+    notices = Post.query.filter(and_(Post.entry_date <= current_datetime, Post.departure_date >= current_datetime, Post.genre=='notice', Post.status=='approved')).order_by(desc(Post.id)).limit(6)
+
+    return render_template('/site/detalhes.html', titulo=titulo, post=post, user=user, category=category, notices=notices, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted), 200
 
 
 @siteBP.route('/filtro')
@@ -173,7 +188,9 @@ def filtro():
     
     posts = paginate.items
 
-    return render_template('site/posts.html', notices=notices, posts=posts, titulo=titulo, name=name, category=category, genre=genre, currentPage=page, paginate=paginate, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted), 200
+    teste = ''
+
+    return render_template('site/posts.html', teste=teste, notices=notices, posts=posts, titulo=titulo, name=name, category=category, genre=genre, currentPage=page, paginate=paginate, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted), 200
 
 
 @siteBP.route('/contato', methods=['GET','POST'])
