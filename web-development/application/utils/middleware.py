86a6807d90857f552059abbdb0aa9db03ee643c4
welisonmenezes <welisonmenezes@hotmail.com>
2019-11-05 12:36:25 -0300

    Add something important

--- web-development/application/utils/middleware.py
+++ web-development/application/utils/middleware.py
@@ -56,5 +56,7 @@ def before_request_func():
             flash('Você não tem acesso à esse recurso', 'warning')
             return redirect(url_for('dashboard.dash'))
 
+    """ Gabriel, aqui o bullying é cruel. """
     if bool(re.search(request.url_root+'admin/tags*', request.url)) or bool(re.search(request.url_root+'tag*', request.url)):
         return redirect(url_for('erro.pageError'))
+    """ Fim do bullying aqui """
