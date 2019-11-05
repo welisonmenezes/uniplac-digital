b358a0163628e1a416926b9cb3f058343ce6ef45
BielSteve <gabrielsteve@uniplaclages.edu.br>
2019-11-04 21:36:35 -0300

    The most arduous battle war fought with great vigor and determination. Tags were eventually won, not a victory won at the hands of just one man, but of fellow warriors. Victory, in the end victory

--- web-development/application/modulos/site/blueprint.py
+++ web-development/application/modulos/site/blueprint.py
@@ -16,13 +16,13 @@ def index():
     categories_highlighted = Category.query.filter((Category.is_highlighted==1)).order_by(desc(Category.id)).all()
     users = User.query.order_by(asc(User.first_name)).all()
     current_datetime = datetime.now()
-    tagg = Tag.query.order_by(desc(Tag.id)).all()
+    tags = Tag.query.order_by(desc(Tag.id)).all()
 
     news = Post.query.outerjoin(User, User.id == Post.user_id).outerjoin(Category, Category.id == Post.category_id).filter(and_(Post.entry_date <= current_datetime, Post.departure_date >= current_datetime, Post.genre=='news', Post.status=='approved')).order_by(desc(Post.id)).limit(10)
     ads = Post.query.filter(and_(Post.entry_date <= current_datetime, Post.departure_date >= current_datetime, Post.genre=='ad', Post.status=='approved')).order_by(desc(Post.id)).limit(6)
     notices = Post.query.filter(and_(Post.entry_date <= current_datetime, Post.departure_date >= current_datetime, Post.genre=='notice', Post.status=='approved')).order_by(desc(Post.id)).limit(6)
 
-    return render_template('site/site.html',tagg=tagg, news=news, ads=ads, notices=notices, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted, users=users), 200
+    return render_template('site/site.html',tags=tags, news=news, ads=ads, notices=notices, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted, users=users), 200
 
 
 @siteBP.route('/noticias')
@@ -98,7 +98,7 @@ def render_post_list_by_type(post_type, title):
     categories_highlighted = Category.query.filter((Category.is_highlighted==1)).order_by(desc(Category.id)).all()
     users = User.query.order_by(asc(User.first_name)).all()
     current_datetime = datetime.now()
-    tagg = Tag.query.order_by(desc(Tag.id)).all()   
+    tags = Tag.query.order_by(desc(Tag.id)).all()   
     
 
     # sidebar avisos
@@ -114,7 +114,7 @@ def render_post_list_by_type(post_type, title):
         genre = '' if (request.args.get('genre') == None) else request.args.get('genre')
         category = '' if (request.args.get('category') == None) else request.args.get('category')
         author  = '' if (request.args.get('author') == None) else request.args.get('author')
-        tagg = '' if(request.args.get('tagg') == None) else request.args.get('tagg')
+        tag = '' if(request.args.get('tag') == None) else request.args.get('tag')
         
         
         # filtro
@@ -128,20 +128,21 @@ def render_post_list_by_type(post_type, title):
         if author:
             filter = filter + (Post.user_id == author,)
     elif(post_type == 'tag'): 
-        tagg = '' if (request.args.get('tagg') == None) else request.args.get('tagg')
-        filter=(Post.tags.any(id=tagg),)
+        filter = (and_(Post.entry_date <= current_datetime, Post.departure_date >= current_datetime, Post.status=='approved'),)
+        tag = '' if (request.args.get('tag') == None) else request.args.get('tag')
+        filter = filter + ( Post.tags.any(id=tag),)
     else:
         # fitro
         filter = (and_(Post.entry_date <= current_datetime, Post.departure_date >= current_datetime, Post.genre==post_type, Post.status=='approved'))
-    print(Post.query.filter(*filter).order_by(desc(Post.id)))
+    #print(Post.query.filter(*filter).order_by(desc(Post.id)))
     # consulta o banco de dados retornando o paginate e os dados
-    paginate = Post.query.outerjoin(User, User.id == Post.user_id).outerjoin(Category, Category.id == Post.category_id).filter(*filter).order_by(desc(Post.id)).paginate(page=page, per_page=10, error_out=False)
+    paginate = Post.query.outerjoin(User, User.id == Post.user_id).outerjoin(Category, Category.id == Post.category_id).filter(*filter).order_by(desc(Post.id)).paginate(page=page, per_page=1, error_out=False)
     posts = paginate.items
 
     if post_type == 'filter':
-        return render_template('site/posts.html', tagg=tagg, notices=notices, posts=posts, titulo=titulo, name=name, category=category, genre=genre, currentPage=page, paginate=paginate, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted, users=users, author=author), 200
+        return render_template('site/posts.html', tags=tags, tag=tag, notices=notices, posts=posts, titulo=titulo, name=name, category=category, genre=genre, currentPage=page, paginate=paginate, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted, users=users, author=author), 200
     else:
-        return render_template('site/posts.html', tagg=tagg, posts=posts, notices=notices, paginate=paginate, currentPage=page, titulo=titulo, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted, users=users), 200
+        return render_template('site/posts.html', tags=tags, tag=tag, posts=posts, notices=notices, paginate=paginate, currentPage=page, titulo=titulo, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted, users=users), 200
 
 
 def render_post_detail_by_type(post_type, title, id):
@@ -151,7 +152,7 @@ def render_post_detail_by_type(post_type, title, id):
     categories_highlighted = Category.query.filter((Category.is_highlighted==1)).order_by(desc(Category.id)).all()
     users = User.query.order_by(asc(User.first_name)).all()
     current_datetime = datetime.now()
-    tagg = Tag.query.order_by(desc(Tag.id)).all()
+    tags = Tag.query.order_by(desc(Tag.id)).all()
 
    
     
@@ -193,4 +194,4 @@ def render_post_detail_by_type(post_type, title, id):
     category = Category.query.filter(Category.id==post.category_id).first()
     notices = Post.query.filter(and_(Post.entry_date <= current_datetime, Post.departure_date >= current_datetime, Post.genre=='notice', Post.status=='approved')).order_by(desc(Post.id)).limit(6)
 
-    return render_template('/site/detalhes.html', tagg=tagg, post=post, user=user, category=category, notices=notices, titulo=titulo, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted, users=users), 200
+    return render_template('/site/detalhes.html', tags=tags, post=post, user=user, category=category, notices=notices, titulo=titulo, configuration=configuration, categories=categories, categories_highlighted=categories_highlighted, users=users), 200
